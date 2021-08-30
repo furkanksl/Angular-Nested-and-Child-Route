d18f80d5b5a1b8e0eeb10eb3ec7d366dc7fabbc5
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2021-08-30 12:17:42 +0300

    created news component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HomeBaseComponent } from './home-base/home-base.component';
+import { NewsComponent } from './news/news.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HomeBaseComponent
+    HomeBaseComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
