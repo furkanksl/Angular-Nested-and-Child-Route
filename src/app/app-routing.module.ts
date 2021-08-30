2faa693aca1d42b11b6e70e8f683b524d107bed8
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2021-08-30 12:14:10 +0300

    craeted home base component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeBaseComponent } from './home-base/home-base.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: "home",
-    component: HomeComponent
+    component: HomeComponent,
+    children: [
+      {
+        path: "",
+        component: HomeBaseComponent,
+      }
+    ]
   },
+
+  {
+    path: "**", component: HomeComponent,
+  }
 ];
 
 @NgModule({
